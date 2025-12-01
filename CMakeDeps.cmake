8d1848728c7799f5bcce5058cb75419bfc3eae27
J-Rios <jrios.github@gmail.com>
2025-12-02 00:26:25 +0100

    [cmake] Fix GLFW build

--- CMakeDeps.cmake
+++ CMakeDeps.cmake
@@ -1,7 +1,7 @@
 
 ### Project Libraries & Tools Dependencies ###
 
-####################################################################################################
+###############################################################################
 
 ### Setup Configuration ###
 
@@ -13,13 +13,13 @@ include(FetchContent)
 set(DIR_DEPS_LIB ${CMAKE_SOURCE_DIR}/deps/libraries)
 set(DIR_DEPS_TOOL ${CMAKE_SOURCE_DIR}/deps/tools)
 
-####################################################################################################
+###############################################################################
 
 ### Libraries To Fetch - GLFW ###
 
 if(IMGUI_BACKEND STREQUAL "GLFW")
     message(STATUS "Fetching GLFW...")
-    set(GLFW_SHARED ON CACHE BOOL "" FORCE)
+    set(GLFW_LIBRARY_TYPE SHARED CACHE STRING "Build GLFW as shared lib" FORCE)
     FetchContent_Declare(
         glfw
         GIT_REPOSITORY https://github.com/glfw/glfw.git
@@ -29,7 +29,7 @@ if(IMGUI_BACKEND STREQUAL "GLFW")
     FetchContent_MakeAvailable(glfw)
 endif()
 
-####################################################################################################
+###############################################################################
 
 ### Libraries To Fetch - SDL2 ###
 
@@ -44,7 +44,7 @@ if(IMGUI_BACKEND STREQUAL "SDL")
     FetchContent_MakeAvailable(SDL2)
 endif()
 
-####################################################################################################
+###############################################################################
 
 ### Libraries To Fetch - Dear Imgui ###
 
@@ -60,56 +60,49 @@ FetchContent_Declare(
 FetchContent_GetProperties(imgui)
 if(NOT imgui_POPULATED)
     FetchContent_MakeAvailable(imgui)
-
-    set(IMGUI_SRC
-        ${IMGUI_CORE}
-        ${imgui_SOURCE_DIR}/imgui.cpp
-        ${imgui_SOURCE_DIR}/imgui_draw.cpp
-        ${imgui_SOURCE_DIR}/imgui_widgets.cpp
-        ${imgui_SOURCE_DIR}/imgui_tables.cpp
-        ${imgui_SOURCE_DIR}/imgui_demo.cpp
-    )
-
-    set(IMGUI_LINK "")
-
-    if(IMGUI_BACKEND STREQUAL "GLFW")
-        set(IMGUI_SRC
-            ${IMGUI_SRC}
-            ${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.cpp
-            ${imgui_SOURCE_DIR}/backends/imgui_impl_opengl3.cpp
-        )
-
-        set(IMGUI_LINK
-            glfw
-            OpenGL::GL
-        )
-    endif()
-
-    if(IMGUI_BACKEND STREQUAL "SDL")
-        set(IMGUI_SRC
-            ${IMGUI_SRC}
-            ${imgui_SOURCE_DIR}/backends/imgui_impl_sdl2.cpp
-            ${imgui_SOURCE_DIR}/backends/imgui_impl_opengl3.cpp
-        )
-
-        set(IMGUI_LINK
-            SDL2::SDL2
-            OpenGL::GL
-        )
-    endif()
-
-    add_library(imgui ${IMGUI_SRC})
-
-    target_include_directories(imgui PUBLIC
-        ${imgui_SOURCE_DIR}
-        ${imgui_SOURCE_DIR}/backends
-    )
-
-    target_link_libraries(imgui PUBLIC ${IMGUI_LINK})
-
 endif()
 
-####################################################################################################
+set(IMGUI_INC
+    ${imgui_SOURCE_DIR}
+    ${imgui_SOURCE_DIR}/backends
+)
+
+set(IMGUI_SRC
+    ${IMGUI_CORE}
+    ${imgui_SOURCE_DIR}/imgui.cpp
+    ${imgui_SOURCE_DIR}/imgui_draw.cpp
+    ${imgui_SOURCE_DIR}/imgui_widgets.cpp
+    ${imgui_SOURCE_DIR}/imgui_tables.cpp
+    ${imgui_SOURCE_DIR}/imgui_demo.cpp
+)
+
+if(IMGUI_BACKEND STREQUAL "GLFW")
+    list(APPEND IMGUI_SRC
+        ${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.cpp
+        ${imgui_SOURCE_DIR}/backends/imgui_impl_opengl3.cpp
+    )
+    set(IMGUI_LINK glfw OpenGL::GL)
+elseif(IMGUI_BACKEND STREQUAL "SDL")
+    list(APPEND IMGUI_SRC
+        ${imgui_SOURCE_DIR}/backends/imgui_impl_sdl2.cpp
+        ${imgui_SOURCE_DIR}/backends/imgui_impl_opengl3.cpp
+    )
+    set(IMGUI_LINK SDL2::SDL2 OpenGL::GL)
+endif()
+
+if(TARGET_PLATFORM STREQUAL "Linux")
+    list(APPEND IMGUI_INC ${X11_INCLUDE_DIR})
+    list(APPEND IMGUI_LINK ${X11_LIBRARIES})
+endif()
+
+if(TARGET imgui)
+    remove_library(imgui)
+endif()
+add_library(imgui ${IMGUI_SRC})
+target_include_directories(imgui PUBLIC ${IMGUI_INC})
+target_link_libraries(imgui PUBLIC ${IMGUI_LINK})
+
+###############################################################################
 
 ### Libraries To Fetch - ... ###
 
@@ -117,7 +110,7 @@ endif()
 # Add Here More Libraries...
 # ...
 
-####################################################################################################
+###############################################################################
 
 ### Tools To Fetch - ... ###
 
@@ -125,4 +118,4 @@ endif()
 # Add Here More Tools...
 # ...
 
-####################################################################################################
+###############################################################################
