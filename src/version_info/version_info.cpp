53df29c4e47fbe917ef7f6841ba00df27cc7f16d
J-Rios <jrios.github@gmail.com>
2025-12-30 20:09:30 +0100

    [version_info] Build type (Debug/Release)

--- src/version_info/version_info.cpp
+++ src/version_info/version_info.cpp
@@ -17,17 +17,18 @@
 
 /* Project Version Information */
 
-const char* const PROJECT_NAME = "@PROJECT_NAME@";
-const char* const PROJECT_VERSION = "v@VERSION_X@.@VERSION_Y@.@VERSION_Z@@VERSION_SUFFIX@";
+const char* const PROJECT_NAME = __PRJ_NAME__;
+const char* const PROJECT_VERSION = __PRJ_VERSION__;
+const char* const BUILD_TYPE = __BUILD_TYPE__;
 const char* const BUILD_DATE = __DATE__;
 const char* const BUILD_TIME = __TIME__;
 
-const bool VERSION_DEV = @VERSION_DEV@;
-const int VERSION_X = @VERSION_X@;
-const int VERSION_Y = @VERSION_Y@;
-const int VERSION_Z = @VERSION_Z@;
+const bool VERSION_DEV = __VERSION_DEV__;
+const int VERSION_X = __VERSION_X__;
+const int VERSION_Y = __VERSION_Y__;
+const int VERSION_Z = __VERSION_Z__;
 
 const char* const PROJECT_TITLE =
-    "@PROJECT_NAME@ v@VERSION_X@.@VERSION_Y@.@VERSION_Z@@VERSION_SUFFIX@ (" __DATE__ " - " __TIME__ ")";
+    __PRJ_NAME__ " " __PRJ_VERSION__ " (" __DATE__ " - " __TIME__ ")";
 
 /*****************************************************************************/
